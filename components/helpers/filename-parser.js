edf5a43690822c8369b788af87f7036a988ec81d
Cody Swartz <ctsstc@gmail.com>
2021-10-12 00:26:00 -0700

    Add: filenameParser, musicFilenameParser

---
+++ components/helpers/filename-parser.js
@@ -0,0 +1,24 @@
+export const filenameParser = (filename) => {
+  const lastPeriodIndex = filename.lastIndexOf('.')
+  const file = filename.substring(0, lastPeriodIndex)
+  const extension = filename.substring(lastPeriodIndex + 1)
+  return { file, extension }
+}
+
+export const musicFilenameParser = (filename) => {
+  const { file, extension } = filenameParser(filename)
+  const nameParts = file.split(' - ')
+  const [artist, song, hash1, hash2] = nameParts
+
+  const isSong = extension === 'mp3'
+
+  return {
+    artist,
+    song,
+    hash1,
+    hash2,
+    filename: file,
+    extension,
+    isSong
+  }
+}
