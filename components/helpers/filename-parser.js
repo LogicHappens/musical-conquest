21c58dbbd8d57cd3f550819d94d866ee7f0125f8
Cody Swartz <ctsstc@gmail.com>
2021-10-16 02:23:21 -0700

    Add main hash to musicFilenameParser

--- components/helpers/filename-parser.js
+++ components/helpers/filename-parser.js
@@ -9,16 +9,17 @@ export const musicFilenameParser = (filename) => {
   const { file, extension } = filenameParser(filename)
   const nameParts = file.split(' - ')
   const [artist, song, hash1, hash2] = nameParts
-
+  const hash = hash2 || hash1
   const isSong = extension === 'mp3'
 
   return {
     artist,
     song,
+    hash,
     hash1,
     hash2,
     filename: file,
     extension,
-    isSong
+    isSong,
   }
 }
