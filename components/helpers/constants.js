40cc4e493a9065fb9fce02f9ba24279ef6dfa3c5
Cody Swartz <ctsstc@gmail.com>
2021-10-16 04:54:45 -0700

    Play a random song quick and dirty Play, pause, stop buttons work

--- components/helpers/constants.js
+++ components/helpers/constants.js
@@ -1,3 +1,4 @@
-export const SONGS_URL =
-  'https://musicalconquest.nyc3.digitaloceanspaces.com/filenames.txt'
+export const BASE_URL = 'https://musicalconquest.nyc3.digitaloceanspaces.com/'
+export const SONGS_BASE_URL = `${BASE_URL}backup/`
+export const SONGS_URL = `${BASE_URL}filenames.txt`
 export const SONGS_LOCAL_STORAGE_KEY = 'filenamesRaw'
--- components/player.js
+++ components/player.js
@@ -1,26 +1,52 @@
 import styles from '../styles/player.module.sass'
-import { useState } from 'react'
 
-
-const Player = () => {
-    const [counter, setCounter] = useState(0)
-
-    return(
-        <div className={styles.player}>
-            <span className={styles.player_screen}>
-                <span className={styles.player_screen_songTimer}>10:53</span>
-                <span className={styles.player_screen_songInfo}>Captain Tip - Touch my tralalala</span>
-            </span>
-            <span className={styles.player_controls}>
-                <span className={styles.player_controls_prev}>Prev</span>
-                <span className={styles.player_controls_play}>Play</span>
-                <span className={styles.player_controls_pause}>pause</span>
-                <span className={styles.player_controls_stop}>Stop</span>
-                <span className={styles.player_controls_next}>Next</span>
-                <span className={styles.player_controls_progress}>Progress</span>
-            </span>
-        </div>
-    )
+const Player = ({
+  song: {
+    song: {
+      artist = 'asdf',
+      song = 'qwerty',
+      hash1,
+      hash2,
+      filename,
+      extension,
+      isSong,
+    } = {},
+  } = {},
+  audio,
+}) => {
+  return (
+    <div className={styles.player}>
+      <span className={styles.player_screen}>
+        <span className={styles.player_screen_songTimer}>10:53</span>
+        <span className={styles.player_screen_songInfo}>
+          {artist} - {song}
+        </span>
+      </span>
+      <span className={styles.player_controls}>
+        <span className={styles.player_controls_prev}>Prev</span>
+        <span
+          className={styles.player_controls_play}
+          onClick={() => audio.play()}
+        >
+          Play
+        </span>
+        <span
+          className={styles.player_controls_pause}
+          onClick={() => audio.pause()}
+        >
+          pause
+        </span>
+        <span
+          className={styles.player_controls_stop}
+          onClick={() => audio.stop()}
+        >
+          Stop
+        </span>
+        <span className={styles.player_controls_next}>Next</span>
+        <span className={styles.player_controls_progress}>Progress</span>
+      </span>
+    </div>
+  )
 }
 
 export default Player
