4ca99a75b98eb62c6d6629f2d9f0e30927a4edce
Cody Swartz <ctsstc@gmail.com>
2021-10-19 02:14:17 -0700

    Add SearchResult component

--- components/search-and-results.js
+++ components/search-and-results.js
@@ -1,33 +1,22 @@
 import styles from '../styles/search.module.sass'
+import SearchResult from './search-result'
 
 const SearchAndResults = () => {
-    return(
-        <div className={styles.search}>
-            <input type="text" placeholder="search..."></input>
-            <ul className={styles.results}>
-                <p>No search results</p>
-                <li className={styles.results_result, styles.results_headers}>
-                    <span className={styles.result_artist}>Artist</span>
-                    <span className={styles.result_name}>Name</span>
-                    <span className={styles.result_actions}>Actions</span>
-                </li>
-                <li className={styles.results_result}>
-                    <span className={styles.result_artist}>Captain Tip</span>
-                    <span className={styles.result_name}>My ding ding dong</span>
-                    <span className={styles.result_actions}>
-                        <a href="#" className={styles.result_actions_add}>+</a>
-                    </span>
-                </li>
-                <li className={styles.results_result}>
-                    <span className={styles.result_artist}>Captain Tip</span>
-                    <span className={styles.result_name}>my shaba daba daba dai</span>
-                    <span className={styles.result_actions}>
-                        <a href="#" className={styles.result_actions_add}>+</a>
-                    </span>
-                </li>
-            </ul>
-        </div>
-    )
+  return (
+    <div className={styles.search}>
+      <input type="text" placeholder="search..."></input>
+      <ul className={styles.results}>
+        <p>No search results</p>
+        <li className={(styles.results_result, styles.results_headers)}>
+          <span className={styles.result_artist}>Artist</span>
+          <span className={styles.result_name}>Name</span>
+          <span className={styles.result_actions}>Actions</span>
+        </li>
+        <SearchResult artist="Captain Tip" songName="My ding ding dong" />
+        <SearchResult artist="Captain Tip" songName="my shaba daba daba dai" />
+      </ul>
+    </div>
+  )
 }
 
 export default SearchAndResults
