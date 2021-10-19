c89750e7813f40667ac12c003cd844bc199e535b
Cody Swartz <ctsstc@gmail.com>
2021-10-19 05:32:15 -0700

    Move initialization into context, add cleanup

--- pages/index.js
+++ pages/index.js
@@ -4,25 +4,16 @@ import Player from '../components/player'
 import SearchAndResults from '../components/search-and-results'
 import Playlist from '../components/playlist'
 import { useCallback, useContext, useEffect, useState } from 'react'
-import {
-  SONGS_BASE_URL,
-  SONGS_LOCAL_STORAGE_KEY,
-  SONGS_URL,
-} from '../components/helpers/constants'
-import axios from 'axios'
-import { musicFilenameParser } from '@/components/helpers/filename-parser'
+import { SONGS_BASE_URL } from '../components/helpers/constants'
 import { Howl } from 'howler'
-import { catalogSongs } from '@/components/helpers/catalog-songs'
 import { useRouter } from 'next/router'
 import { Context } from '../components/helpers/context'
 
 export default function Home() {
-  const { currentSong, setCurrentSong } = useContext(Context)
+  const { catalog, currentSongHash, setCurrentSong } = useContext(Context)
   const [audio, setAudio] = useState()
-  const [catalog, setCatalog] = useState([])
   const router = useRouter()
 
-  const parseFiles = (files) => files.split('\n').map(musicFilenameParser)
   const getUrlSongHash = useCallback(
     () => router.asPath.split('/#')[1],
     [router.asPath],
@@ -31,30 +22,9 @@ export default function Home() {
   const [lastHash, setLastHash] = useState(getUrlSongHash())
 
   useEffect(() => {
-    const filenames = localStorage.getItem(SONGS_LOCAL_STORAGE_KEY)
-    if (filenames) {
-      const songs = parseFiles(filenames)
-      const catalog = catalogSongs(songs)
-      const keys = Array.from(catalog.keys())
-      const loadKey = getUrlSongHash()
-      const loadExists = loadKey && catalog.has(loadKey)
-      const randomKey = keys[Math.floor(Math.random() * keys.length)]
-      const songKey = loadExists ? loadKey : randomKey
-
-      setCatalog(catalog)
-      setCurrentSongHash(songKey)
-      return
-    }
-
-    axios.get(SONGS_URL).then(({ data }) => {
-      localStorage.setItem(SONGS_LOCAL_STORAGE_KEY, data)
-      parseFiles(data)
-    })
-  }, [])
-
-  useEffect(() => {
     if (currentSongHash === '') return
     const { song } = catalog.get(currentSongHash)
+    setCurrentSong(song)
 
     const songUrl =
       SONGS_BASE_URL + encodeURIComponent(song.filename) + '.' + song.extension
@@ -73,7 +43,11 @@ export default function Home() {
       router.push(`/#${song.hash}`, undefined, { shallow: true })
       setLastHash(song.hash)
     }
-  }, [catalog, currentSongHash, lastHash, router])
+
+    return function cleanup() {
+      howl.unload()
+    }
+  }, [catalog, currentSongHash, lastHash, router, setCurrentSong])
 
   return (
     <div className={styles.container}>
