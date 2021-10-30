2cc2f6ccf4b71d9a92b312da9c5bca6762468db6
Germán Campos <sotkra@gmail.com>
2021-10-30 15:30:12 -0500

    cant even remember what

--- components/search-result.js
+++ components/search-result.js
@@ -6,17 +6,18 @@ const SearchResult = ({ hash, artist, songName }) => {
   const { setCurrentSongHash } = useContext(Context)
 
   return (
-    <li className={styles.results_result}>
-      <span className={styles.result_artist}>{artist}</span>
-      <span className={styles.result_name}>{songName}</span>
-      <span className={styles.result_actions}>
-        <button
-          className={styles.result_actions_play}
-          onClick={() => setCurrentSongHash(hash)}
-        ></button>
-        <button className={styles.result_actions_add}></button>
-      </span>
-    </li>
+
+      <li className={styles.results_result}>
+        <span className={styles.result_artist}>{artist}</span>
+        <span className={styles.result_name}>{songName}</span>
+        <span className={styles.result_actions}>
+          <button
+            className={styles.result_actions_play}
+            onClick={() => setCurrentSongHash(hash)}
+          ></button>
+          <button className={styles.result_actions_add}></button>
+        </span>
+      </li>
   )
 }
 
