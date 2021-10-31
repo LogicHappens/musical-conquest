3cf0d085d94f7cabad37fa0b6900cad685659956
Cody Swartz <ctsstc@gmail.com>
2021-10-31 16:30:30 -0700

    Fix PlaylistSong issue if there's no image

--- components/playlist/playlist-song.js
+++ components/playlist/playlist-song.js
@@ -12,12 +12,16 @@ const PlaylistSong = ({
       <span className={styles.song_artist}>{artist}</span>
       <span className={styles.song_name}>{song}</span>
       <span className={styles.song_image}>
-        <img
-          width="20"
-          height="20"
-          alt="artist image"
-          src={SONGS_BASE_URL + images[0].filename + '.' + images[0].extension}
-        />
+        {images[0] && (
+          <img
+            width="20"
+            height="20"
+            alt="artist image"
+            src={
+              SONGS_BASE_URL + images[0].filename + '.' + images[0].extension
+            }
+          />
+        )}
       </span>
       <span className={styles.song_actions}>
         <a href="#" className={styles.playlist_song_actions_remove}>
