3aabf774c9606a38a63a88245e5382b5d2d0c7e0
Cody Swartz <ctsstc@gmail.com>
2021-11-04 16:45:23 -0700

    Add analytics Fixes #25
    Hopefully I did this right
    
    Going to try GoatCounter
    
    This should count on every song change as a hit

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import { Provider } from '@/components/helpers/context'
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider>
+      <Head>
+        <script
+          data-goatcounter="https://musicalconquest.goatcounter.com/count"
+          async
+          src="//gc.zgo.at/count.js"
+        ></script>
+      </Head>
       <Component {...pageProps} />
     </Provider>
   )
